23a95434888783274b825133a10747954aeccf76
Pedro Meira <pedrohmeira@icloud.com>
2024-10-22 17:06:06 -0300

    adicionar novas features

--- src/server.ts
+++ src/server.ts
@@ -1,94 +1,101 @@
 import express from "express"
 import { prisma } from "./lib/prisma"
+import cors from 'cors'
 
 const PORT = 3333
 
 const app = express()
 
+app.use(cors())
+
 app.use(express.json())
 
 app.post("/option", async (req, res) => {
-   const { name } = req.body
+    const { name } = req.body
 
-   if(!name) {
-    return res.status(422).json({
-        error: "Name é obrigatório"
-    })
-   }
+    if (!name) {
+        return res.status(422).json({
+            error: "Name é obrigatório"
+        })
+    }
 
-   const defaultVoting = await prisma.voting.findFirst({
+    const defaultVoting = await prisma.voting.findFirst({
         where: {
             name: "default"
         }
-   })
-
-   let votingId: null | string = null
-
-   if(!defaultVoting?.id) {
-    const createdVoting = await prisma.voting.create({
-        data: {
-            name: "default"
-        }
     })
 
-    votingId = createdVoting.id
-   } else {
-    votingId = defaultVoting.id
-   }
+    let votingId: null | string = null
 
-   await prisma.option.create({
+    if (!defaultVoting?.id) {
+        const createdVoting = await prisma.voting.create({
+            data: {
+                name: "default"
+            }
+        })
+
+        votingId = createdVoting.id
+    } else {
+        votingId = defaultVoting.id
+    }
+
+    await prisma.option.create({
         data: {
             name,
             votingId
         }
-   })
+    })
 
-   return res.status(200).json({
+    return res.status(200).json({
         msg: "Option created"
-   })
+    })
 })
 
 app.post("/vote/:optionId", async (req, res) => {
     const { participantName } = req.body
-    const optionId = req.query
- 
-    if(!participantName) {
-     return res.status(422).json({
-         error: "participantName é obrigatório"
-     })
+    const {optionId} = req.params
+
+    console.log("> optionId",optionId);
+    
+
+    if (!participantName) {
+        return res.status(422).json({
+            error: "participantName é obrigatório"
+        })
     }
 
-    if(!optionId) {
+    if (!optionId) {
         return res.status(422).json({
             error: "optionId é obrigatório"
         })
-       }
- 
+    }
+
     const option = await prisma.option.findFirst({
-         where: {
-             id: optionId
-         }
+        where: {
+            id: optionId
+        }
     })
 
-    if(!option) {
+    if (!option) {
         return res.status(404).json({
             error: "Opção não encontrada"
-        }) 
+        })
     }
- 
-    await prisma.vote.create({
-         data: {
-             participantName,
-             optionId: option.id
-         }
-    })
- 
-    return res.status(200).json({
-         msg: "Vote saved"
-    })
- })
 
- app.get("/voting/metrics", async (req, res) => {
+    await prisma.vote.create({
+        data: {
+            participantName,
+            optionId: option.id
+        }
+    })
+
+    return res.status(200).json({
+        msg: "Vote saved"
+    })
+})
+
+app.get("/voting/metrics", async (req, res) => {
+
 
     const voting = await prisma.voting.findFirst({
         where: {
@@ -96,32 +103,71 @@ app.post("/vote/:optionId", async (req, res) => {
         }
     })
 
-    if(!voting) {
+    if (!voting) {
         return res.status(404).json({
             error: "Voting não encontrado"
         })
     }
-    
-    const voteCounts = await prisma.option.groupBy({
-        by: ['id', 'name'],
+
+    const optionsWithVoteCounts = await prisma.option.findMany({
         where: {
-          votingId: voting.id,
+            votingId: voting.id,
         },
-        _count: {
-          id: true
+        select: {
+            id: true,
+            name: true,
+            _count: {
+                select: {
+                    Vote: true,
+                },
+            },
         },
-      });
- 
-    return res.status(200).json({
-        result: voteCounts.map(option => ({
-            id: option.id,
-            name: option.name,
-            voteCount: option._count.id,
-          }))
+    });
+
+    const totalVotes = await prisma.vote.count({
+        where: {
+            option: {
+                votingId: voting.id
+            }
+        }
     })
- })
+
+    return res.status(200).json({
+        votes: optionsWithVoteCounts.map(option => ({
+            optionId: option.id,
+            optionName: option.name,
+            voteCount: option._count.Vote,
+          })),
+          totalVotes
+    })
+
+})
+
+app.get("/voting/options", async (req, res) => {
+    const voting = await prisma.voting.findFirst({
+        where: {
+            name: "default"
+        }
+    })
+
+    if (!voting) {
+        return res.status(404).json({
+            error: "Voting não encontrado"
+        })
+    }
+
+    const options = await prisma.option.findMany({
+        where: {
+            votingId: voting.id
+        }
+    })
+
+    return res.status(200).json({
+        options: options
+    })
+})
 
 
 app.listen(PORT, () => {
-    console.log(`> Server running on port ${PORT}`);  
+    console.log(`> Server running on port ${PORT}`);
 })
